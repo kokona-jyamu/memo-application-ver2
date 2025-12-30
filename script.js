2e7f198bd68af9d1fd0b63a647203a88f5687ef2
kokona <kokonajyamuojisann@gmail.com>
2025-12-30 21:13:31 +0900

    update html css js

---
+++ script.js
@@ -0,0 +1,8 @@
+const memoInput = document.getElementById("memoInput");
+const addBtn = document.getElementById("addBtn");
+const memoList = document.getElementById("memoList");
+
+$(function () {
+  $('.container h1').delay(300).fadeIn(1000);
+  $('.container p').delay(400).fadeIn(1200);
+});
